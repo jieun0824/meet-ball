8fc8f8ee11f89196ee8322bc41be510b0e551a81
Taegon <taegon@cusee.net>
2024-02-02 21:45:33 +0900

    first create making

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,3 +1,61 @@
 export default function CreatePage() {
-    return <div>Create</div>
+  async function createMeet(formData: FormData) {
+    'use server';
+
+    const meetingStartHour = formData.get('meetingStartHour');
+    const meetingStartMin = formData.get('meetingStartMin');
+    const meetingEndHour = formData.get('meetingEndHour');
+    const meetingEndMin = formData.get('meetingEndMin');
+
+    console.log({
+      meetingEndHour,
+      meetingEndMin,
+      meetingStartHour,
+      meetingStartMin,
+    });
+  }
+
+  const hours = Array.from({ length: 24 }, (_, index) => index + 1);
+  const mins = [0, 30];
+
+  return (
+    <>
+      <form action={createMeet}>
+        <div>
+          <p>회의 시간대</p>
+          <select value="meetingStartHour">
+            {hours.map(hour => (
+              <option key={hour} value={hour}>
+                {hour}
+              </option>
+            ))}
+          </select>
+          <p>:</p>
+          <select value="meetingStartMin">
+            {mins.map(mins => (
+              <option key={mins} value={mins}>
+                {mins}
+              </option>
+            ))}
+          </select>
+          <p>-</p>
+          <select value="meetingEndHour">
+            {hours.map(hour => (
+              <option key={hour} value={hour}>
+                {hour}
+              </option>
+            ))}
+          </select>
+          <p>:</p>
+          <select value="meetingEndMin">
+            {mins.map(mins => (
+              <option key={mins} value={mins}>
+                {mins}
+              </option>
+            ))}
+          </select>
+        </div>
+      </form>
+    </>
+  );
 }
