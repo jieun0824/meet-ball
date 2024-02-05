322e605597752a1e143be22905c7980ce3591243
Gwon Hong <gwonhong.dev@gmail.com>
2024-02-06 00:31:53 +0900

    add: next-auth and prisma dependency and base codes for authentication

---
+++ src/auth.ts
@@ -0,0 +1,12 @@
+import NextAuth from "next-auth";
+import Google from "@auth/core/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import prisma from "@/lib/prisma";
+
+export const {
+  handlers: { GET, POST },
+  auth,
+} = NextAuth({
+  providers: [Google],
+  adapter: PrismaAdapter(prisma),
+});
