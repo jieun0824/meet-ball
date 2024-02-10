da181a5f0d271dd2f38b838d8bd505cc75252b09
jieun <suspen064@gmail.com>
2024-02-10 15:58:13 +0900

    fix: replace api/signin/google -> /login

--- src/app/(main)/login/page.tsx
+++ src/app/(main)/login/page.tsx
@@ -1,9 +1,9 @@
-import { auth } from '@/auth';
+import { POST, GET, auth } from '@/auth';
 import Link from 'next/link';
 import { redirect } from 'next/navigation';
 import Image from 'next/image';
 import InfoCarousel from '@/components/login/info-carousel';
-
+import { signIn } from 'next-auth/react';
 //https://developers.google.com/identity/branding-guidelines?hl=ko <-design guidelines
 
 export default async function LoginPage() {
@@ -12,20 +12,6 @@ export default async function LoginPage() {
   return (
     <div className="flex flex-col justify-center items-center h-[80dvh]">
       <InfoCarousel />
-      <Link
-        href="/api/auth/signin/google"
-        className="flex justify-center items-center bg-white rounded-lg pr-4 pl-4 pt-2 pb-2"
-      >
-        <Image
-          src="/icon/GoogleLogo.svg"
-          alt="google logo"
-          width={35}
-          height={35}
-        />
-        <div className="text-black font-roboto ml-2">
-          Google 계정으로 로그인
-        </div>
-      </Link>
     </div>
   );
 }
--- src/components/login/info-carousel.tsx
+++ src/components/login/info-carousel.tsx
@@ -2,19 +2,37 @@
 
 import useCarousel from '@/hooks/useCarousel';
 import InfoCard from './info-card';
+import Image from 'next/image';
+import { signIn } from 'next-auth/react';
 
 export default function InfoCarousel() {
   const { currentSlide, onMouseDown, onMouseUp, cardRef } = useCarousel(3, 320);
   return (
-    <div className="mt-8 w-72 mb-4 overflow-hidden relative flex h-full">
-      <div
-        className="grid grid-cols-3 gap-80 font-semibold "
-        onMouseDown={onMouseDown}
-        onMouseUp={onMouseUp}
-        ref={cardRef}
-      >
-        <InfoCard />
+    <>
+      <div className="mt-8 w-72 mb-4 overflow-hidden relative flex h-full">
+        <div
+          className="grid grid-cols-3 gap-80 font-semibold "
+          onMouseDown={onMouseDown}
+          onMouseUp={onMouseUp}
+          ref={cardRef}
+        >
+          <InfoCard />
+        </div>
       </div>
-    </div>
+      <button
+        onClick={() => signIn('google')}
+        className="flex justify-center items-center bg-white rounded-lg pr-4 pl-4 pt-2 pb-2"
+      >
+        <Image
+          src="/icon/GoogleLogo.svg"
+          alt="google logo"
+          width={35}
+          height={35}
+        />
+        <div className="text-black font-roboto ml-2">
+          Google 계정으로 로그인
+        </div>
+      </button>
+    </>
   );
 }
