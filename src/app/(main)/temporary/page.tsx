d6ba01ae1f7e3791633a9910ea6c5ba4cd76a671
Gwon Hong <gwonhong.dev@gmail.com>
2024-02-12 17:30:24 +0900

    add: delete meet button for testing purpose

--- src/app/(main)/temporary/page.tsx
+++ src/app/(main)/temporary/page.tsx
@@ -1,28 +1,52 @@
 'use client';
 
 import { useState } from 'react';
-import { createMeet } from '@/controllers/meet';
-import { Meet, MeetType } from '@prisma/client';
+import { createMeet, deleteMeet } from '@/controllers/meet';
+import { MeetType } from '@prisma/client';
 
 export default function TemporaryPage() {
-  const [result, setResult] = useState<Meet | null>(null);
+  const [inputId, setInputId] = useState<string>('');
+  const [result, setResult] = useState<string>('');
   return (
     <>
-      <button
-        onClick={async () => {
-          const meet = await createMeet({
-            name: 'test',
-            description: 'test',
-            meetType: MeetType.DATES,
-            datesOrDays: ['2022-10-10'],
-          });
-          setResult(meet);
-        }}
-      >
-        createTestMeet
-      </button>
       <section>
-        <h1>Created Meet</h1>
+        <h1>Buttons</h1>
+        <button
+          onClick={async () => {
+            const meet = await createMeet({
+              name: 'test',
+              description: 'test',
+              meetType: MeetType.DATES,
+              datesOrDays: ['2022-10-10'],
+            });
+            setResult(JSON.stringify(meet));
+          }}
+        >
+          Create Test Meet
+        </button>
+        <div>
+          <label>MeetId: </label>
+          <input
+            id="meetId"
+            value={inputId}
+            onChange={event => {
+              setInputId(event.target.value);
+            }}
+            className="text-black"
+          />
+        </div>
+        <button
+          onClick={async () => {
+            await deleteMeet(inputId);
+            setResult('delete success.');
+          }}
+        >
+          Delete Meet
+        </button>
+      </section>
+
+      <section>
+        <h1>Result</h1>
         {result ? JSON.stringify(result) : null}
       </section>
     </>
