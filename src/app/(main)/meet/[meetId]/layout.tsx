835a42a9be3a39b2b6d3e1d959779ed262e811ec
Taegon <taegon@cusee.net>
2024-02-22 17:48:58 +0900

    resolve font problem, add font to layout.tsx remove public/font

--- src/app/(main)/meet/[meetId]/layout.tsx
+++ src/app/(main)/meet/[meetId]/layout.tsx
@@ -1,7 +1,13 @@
-import { TimeTableFont } from '../../../../../public/font/timeTableFont';
+import { Montserrat } from 'next/font/google';
+
+const montserrat = Montserrat({
+  subsets: ['latin'],
+  weight: '400',
+  variable: '--font-montserrat',
+});
 
 export default function Layout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
-  return <div className={TimeTableFont}>{children}</div>;
+  return <div className={montserrat.className}>{children}</div>;
 }
