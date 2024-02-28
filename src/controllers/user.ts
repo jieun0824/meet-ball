22811c2582afe77216ca4aba1dc148ed50f6d761
Gwon Hong <gwonhong.dev@gmail.com>
2024-02-28 19:17:57 +0900

    add: getUserIdsByEmails method to user.ts

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -2,11 +2,28 @@
 
 import type { AuthenticatedUser } from 'next-auth';
 import { getCurrentUser } from '@/lib/authentication';
+import prisma from '@/lib/prisma';
 
 export async function getMyInfo(): Promise<AuthenticatedUser> {
   try {
     const currentUser = await getCurrentUser();
     return currentUser;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
+
+export async function getUserIdsByEmails(emails: string[]): Promise<string[]> {
+  try {
+    const users = await prisma.user.findMany({
+      where: {
+        email: {
+          in: emails,
+        },
+      },
+    });
+    return users.map(user => user.id);
   } catch (error) {
     console.error(error);
     throw error;
