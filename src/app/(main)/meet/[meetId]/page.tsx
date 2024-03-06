c50bbb25776541ba4daf836a3d5fd1887c3b2dfd
Gwon Hong <gwonhong.dev@gmail.com>
2024-03-06 17:17:33 +0900

    add: refactored slightly

--- src/app/(main)/meet/[meetId]/page.tsx
+++ src/app/(main)/meet/[meetId]/page.tsx
@@ -11,19 +11,11 @@ export default async function MeetPage({
 }: {
   params: { meetId: string };
 }) {
-  const {
-    name,
-    description,
-    datesOrDays,
-    meetType,
-    startTime,
-    endTime,
-    participants,
-  } = await getMeetWithParticipants(params.meetId);
+  const meet = await getMeetWithParticipants(params.meetId);
 
   function combineTimeTables(participants: ParticipantsOnMeets[]) {
     const combinedTimeTable: CombinedTimeTable = {};
-    for (const key of datesOrDays) {
+    for (const key of meet.datesOrDays) {
       combinedTimeTable[key] = {};
     }
 
@@ -38,12 +30,12 @@ export default async function MeetPage({
     return combinedTimeTable;
   }
 
-  const combinedTimeTable = combineTimeTables(participants);
+  const combinedTimeTable = combineTimeTables(meet.participants);
 
   return (
     <div className="pb-8 px-20">
       <div className="flex items-center w-full">
-        <p className="text-xl mt-3 grow">{name}</p>
+        <p className="text-xl mt-3 grow">{meet.name}</p>
         <Link href={`/edit/${params.meetId}`}>
           <GearIcon />
         </Link>
@@ -52,15 +44,15 @@ export default async function MeetPage({
         </Link>
       </div>
       <p className="text-sm h-[40px] border rounded-lg p-2 mt-3 w-full">
-        {description}
+        {meet.description}
       </p>
       <TimeTableComponent
-        startTime={startTime}
-        endTime={endTime}
-        datesOrDays={datesOrDays}
-        type={meetType}
+        startTime={meet.startTime}
+        endTime={meet.endTime}
+        datesOrDays={meet.datesOrDays}
+        type={meet.meetType}
         timetable={combinedTimeTable}
-        participantsNum={participants.length}
+        participantsNum={meet.participants.length}
       />
     </div>
   );
