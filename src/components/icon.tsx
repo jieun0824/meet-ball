565a435d29614586e2973d1c35731029cc9fde3d
Gwon Hong <gwonhong.dev@gmail.com>
2024-03-06 15:45:04 +0900

    add: icon for invite button

--- src/components/icon.tsx
+++ src/components/icon.tsx
@@ -1,13 +1,13 @@
 import { GoClockFill as ClockIcon } from 'react-icons/go';
-import { BsFillPeopleFill as PeopleIcon } from 'react-icons/bs';
-import { FaArrowAltCircleLeft as LeftIcon } from 'react-icons/fa';
-import { FaArrowAltCircleRight as RightIcon } from 'react-icons/fa';
+import { BsFillPeopleFill as PeopleIcon, BsPersonFillAdd as AddPersonIcon } from 'react-icons/bs';
+import { FaArrowAltCircleLeft as LeftIcon, FaArrowAltCircleRight as RightIcon } from 'react-icons/fa';
 import { IoMdMore as MoreIcon } from 'react-icons/io';
 import { FaCircle as CarouselIndexIcon } from 'react-icons/fa6';
 
 export {
   ClockIcon,
   PeopleIcon,
+  AddPersonIcon,
   LeftIcon,
   RightIcon,
   MoreIcon,
