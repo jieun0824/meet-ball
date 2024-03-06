418c16568d780df26e6f03de21d7de5c41db405d
Gwon Hong <gwonhong.dev@gmail.com>
2024-03-06 16:51:01 +0900

    add: gear icon

--- src/components/icon.tsx
+++ src/components/icon.tsx
@@ -1,6 +1,13 @@
 import { GoClockFill as ClockIcon } from 'react-icons/go';
-import { BsFillPeopleFill as PeopleIcon, BsPersonFillAdd as AddPersonIcon } from 'react-icons/bs';
-import { FaArrowAltCircleLeft as LeftIcon, FaArrowAltCircleRight as RightIcon } from 'react-icons/fa';
+import {
+  BsFillPeopleFill as PeopleIcon,
+  BsPersonFillAdd as AddPersonIcon,
+  BsFillGearFill as GearIcon,
+} from 'react-icons/bs';
+import {
+  FaArrowAltCircleLeft as LeftIcon,
+  FaArrowAltCircleRight as RightIcon,
+} from 'react-icons/fa';
 import { IoMdMore as MoreIcon } from 'react-icons/io';
 import { FaCircle as CarouselIndexIcon } from 'react-icons/fa6';
 
@@ -8,6 +15,7 @@ export {
   ClockIcon,
   PeopleIcon,
   AddPersonIcon,
+  GearIcon,
   LeftIcon,
   RightIcon,
   MoreIcon,
