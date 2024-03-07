88725788a4e7626d07aad819c96690c7553ae696
Gwon Hong <gwonhong.dev@gmail.com>
2024-03-07 18:30:03 +0900

    fix: refactored code and removed unnecessary div nesting

--- src/app/(main)/meet/[meetId]/edit/page.tsx
+++ src/app/(main)/meet/[meetId]/edit/page.tsx
@@ -6,26 +6,23 @@ export default async function EditTimetablePage({
 }: {
   params: { meetId: string };
 }) {
-  const { name, description, datesOrDays, meetType, startTime, endTime } =
-    await getMeetWithParticipants(params.meetId);
+  const meet = await getMeetWithParticipants(params.meetId);
   const userTimeTable = (await getMyTimeTable(params.meetId)) ?? {};
-  for (const key of datesOrDays) {
+  for (const key of meet.datesOrDays) {
     userTimeTable[key] = [];
   }
 
   return (
     <div className="pb-8 px-20">
-      <div className="flex flex-col items-center justify-center">
-        <p className="text-xl mt-3 w-full">{name}</p>
-        <p className="text-sm h-[40px] border rounded-lg p-2 mt-3 w-full">
-          {description}
-        </p>
-      </div>
+      <p className="text-xl mt-3 w-full">{meet.name}</p>
+      <p className="text-sm h-[40px] border rounded-lg p-2 mt-3 w-full">
+        {meet.description}
+      </p>
       <TimeTableEditor
-        startTime={startTime}
-        endTime={endTime}
-        datesOrDays={datesOrDays}
-        type={meetType}
+        startTime={meet.startTime}
+        endTime={meet.endTime}
+        datesOrDays={meet.datesOrDays}
+        type={meet.meetType}
         timeTable={userTimeTable}
       />
     </div>
