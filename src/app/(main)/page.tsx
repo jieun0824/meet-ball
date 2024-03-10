a09f6adfce1c5d9a6654257b84d12d685a28b6d1
jieun <suspen064@gmail.com>
2024-03-10 10:23:34 +0900

    fix: fix mainpage card

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,22 +1,72 @@
 import DatesSelector from '@/components/calendar/dates-selector';
-import MainCarousel from '@/components/card/main-carousel';
-import mockupSchedules from '@/scheduleData.json';
 import { auth } from '@/auth';
+import MainCard from '@/components/card/main-card';
+import {
+  getMyManagingMeets,
+  getMyParticipatingMeets,
+} from '@/controllers/meet';
+import Link from 'next/link';
 
 async function fetchData() {
-  return mockupSchedules;
+  const myManagingMeets = await getMyManagingMeets();
+  const myParticipatingMeets = await getMyParticipatingMeets();
+  return [myManagingMeets, myParticipatingMeets];
 }
 
 export default async function MainPage() {
   const session = await auth();
-  const schedules = await fetchData();
+  const [myManagingMeets, myParticipatingMeets] = await fetchData();
   return (
     <div className="flex mobile:flex-col items-center mobile:px-4 laptop:px-10 laptop:justify-evenly laptop:gap-10 desktop:justify-evenly">
       <DatesSelector />
       {session && (
-        <div className="laptop:flex laptop:flex-col">
-          <MainCarousel title={'생성한 이벤트'} data={schedules} />
-          <MainCarousel title={'참여중인 이벤트'} data={schedules} />
+        <div className="flex flex-col mb-10">
+          <div className="my-10">
+            <div className="mb-[20px]">최근 생성 이벤트</div>
+            <Link
+              href={`/meet/${myManagingMeets[myManagingMeets.length - 1].id}`}
+            >
+              <MainCard
+                meetName={myManagingMeets[myManagingMeets.length - 1].name}
+                description={
+                  myManagingMeets[myManagingMeets.length - 1].description
+                }
+                startTime={
+                  myManagingMeets[myManagingMeets.length - 1].startTime
+                }
+                endTime={myManagingMeets[myManagingMeets.length - 1].endTime}
+                // participants={
+                //   myManagingMeets[myManagingMeets.length - 1].participants
+                // }
+              />
+            </Link>
+          </div>
+          <div>
+            <div className="mb-[20px]">최근 참여 이벤트</div>
+            <Link
+              href={`/meet/${myParticipatingMeets[myParticipatingMeets.length - 1].id}`}
+            >
+              <MainCard
+                meetName={
+                  myParticipatingMeets[myParticipatingMeets.length - 1].name
+                }
+                description={
+                  myParticipatingMeets[myParticipatingMeets.length - 1]
+                    .description
+                }
+                startTime={
+                  myParticipatingMeets[myParticipatingMeets.length - 1]
+                    .startTime
+                }
+                endTime={
+                  myParticipatingMeets[myParticipatingMeets.length - 1].endTime
+                }
+                // participants={
+                //   myParticipatingMeets[myParticipatingMeets.length - 1].participants
+                // }
+              />
+            </Link>
+          </div>
         </div>
       )}
     </div>
