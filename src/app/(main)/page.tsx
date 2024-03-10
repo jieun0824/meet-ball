554b8f2527da5a95b149a4650849919eee43c4e7
Gwon Hong <gwonhong.dev@gmail.com>
2024-03-10 12:14:07 +0900

    fix: error when not logged in

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,5 +1,4 @@
 import DatesSelector from '@/components/calendar/dates-selector';
-import { auth } from '@/auth';
 import MainCard from '@/components/card/main-card';
 import {
   getMyManagingMeets,
@@ -7,60 +6,63 @@ import {
 } from '@/controllers/meet';
 import Link from 'next/link';
 
-async function fetchData() {
-  const myManagingMeets = await getMyManagingMeets();
-  const myParticipatingMeets = await getMyParticipatingMeets();
-  return [myManagingMeets, myParticipatingMeets];
-}
-
 export default async function MainPage() {
-  const session = await auth();
-  const [myManagingMeets, myParticipatingMeets] = await fetchData();
+  let myManagingMeets, myParticipatingMeets;
+  try {
+    myManagingMeets = await getMyManagingMeets();
+    myParticipatingMeets = await getMyParticipatingMeets();
+  } catch (error) {
+    console.log(error);
+  }
 
-  const managingMeetsContent = session && myManagingMeets.length !== 0 && (
-    <div className="my-10">
-      <div className="mb-[20px]">최근 생성 이벤트</div>
-      <Link href={`/meet/${myManagingMeets[myManagingMeets.length - 1].id}`}>
-        <MainCard
-          meetName={myManagingMeets[myManagingMeets.length - 1].name}
-          description={myManagingMeets[myManagingMeets.length - 1].description}
-          startTime={myManagingMeets[myManagingMeets.length - 1].startTime}
-          endTime={myManagingMeets[myManagingMeets.length - 1].endTime}
-        />
-      </Link>
-    </div>
-  );
-
-  const participatingMeetsContent = session &&
-    myParticipatingMeets.length !== 0 && (
-      <div className="my-10">
-        <div className="mb-[20px]">최근 참여 이벤트</div>
-        <Link
-          href={`/meet/${myParticipatingMeets[myParticipatingMeets.length - 1].id}`}
-        >
-          <MainCard
-            meetName={
-              myParticipatingMeets[myParticipatingMeets.length - 1].name
-            }
-            description={
-              myParticipatingMeets[myParticipatingMeets.length - 1].description
-            }
-            startTime={
-              myParticipatingMeets[myParticipatingMeets.length - 1].startTime
-            }
-            endTime={
-              myParticipatingMeets[myParticipatingMeets.length - 1].endTime
-            }
-          />
-        </Link>
-      </div>
-    );
   return (
     <div className="flex mobile:flex-col items-center mobile:px-4 laptop:px-10 laptop:justify-evenly laptop:gap-10 desktop:justify-evenly">
       <DatesSelector />
       <div className="flex flex-col">
-        {managingMeetsContent}
-        {participatingMeetsContent}
+        {myManagingMeets ? (
+          <div className="my-10">
+            <div className="mb-[20px]">최근 생성 이벤트</div>
+            <Link
+              href={`/meet/${myManagingMeets[myManagingMeets.length - 1].id}`}
+            >
+              <MainCard
+                meetName={myManagingMeets[myManagingMeets.length - 1].name}
+                description={
+                  myManagingMeets[myManagingMeets.length - 1].description
+                }
+                startTime={
+                  myManagingMeets[myManagingMeets.length - 1].startTime
+                }
+                endTime={myManagingMeets[myManagingMeets.length - 1].endTime}
+              />
+            </Link>
+          </div>
+        ) : null}
+        {myParticipatingMeets ? (
+          <div className="my-10">
+            <div className="mb-[20px]">최근 참여 이벤트</div>
+            <Link
+              href={`/meet/${myParticipatingMeets[myParticipatingMeets.length - 1].id}`}
+            >
+              <MainCard
+                meetName={
+                  myParticipatingMeets[myParticipatingMeets.length - 1].name
+                }
+                description={
+                  myParticipatingMeets[myParticipatingMeets.length - 1]
+                    .description
+                }
+                startTime={
+                  myParticipatingMeets[myParticipatingMeets.length - 1]
+                    .startTime
+                }
+                endTime={
+                  myParticipatingMeets[myParticipatingMeets.length - 1].endTime
+                }
+              />
+            </Link>
+          </div>
+        ) : null}
       </div>
     </div>
   );
