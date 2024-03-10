e78376d7dea17f264faa2573616d6b8df4fde340
jieun <suspen064@gmail.com>
2024-03-10 10:42:16 +0900

    add: if there's no data in main

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -16,59 +16,54 @@ async function fetchData() {
 export default async function MainPage() {
   const session = await auth();
   const [myManagingMeets, myParticipatingMeets] = await fetchData();
+
+  const managingMeetsContent = session && myManagingMeets.length !== 0 && (
+    <div className="flex flex-col mb-10">
+      <div className="my-10">
+        <div className="mb-[20px]">최근 생성 이벤트</div>
+        <Link href={`/meet/${myManagingMeets[myManagingMeets.length - 1].id}`}>
+          <MainCard
+            meetName={myManagingMeets[myManagingMeets.length - 1].name}
+            description={
+              myManagingMeets[myManagingMeets.length - 1].description
+            }
+            startTime={myManagingMeets[myManagingMeets.length - 1].startTime}
+            endTime={myManagingMeets[myManagingMeets.length - 1].endTime}
+          />
+        </Link>
+      </div>
+    </div>
+  );
+
+  const participatingMeetsContent = session &&
+    myParticipatingMeets.length !== 0 && (
+      <div>
+        <div className="mb-[20px]">최근 참여 이벤트</div>
+        <Link
+          href={`/meet/${myParticipatingMeets[myParticipatingMeets.length - 1].id}`}
+        >
+          <MainCard
+            meetName={
+              myParticipatingMeets[myParticipatingMeets.length - 1].name
+            }
+            description={
+              myParticipatingMeets[myParticipatingMeets.length - 1].description
+            }
+            startTime={
+              myParticipatingMeets[myParticipatingMeets.length - 1].startTime
+            }
+            endTime={
+              myParticipatingMeets[myParticipatingMeets.length - 1].endTime
+            }
+          />
+        </Link>
+      </div>
+    );
   return (
     <div className="flex mobile:flex-col items-center mobile:px-4 laptop:px-10 laptop:justify-evenly laptop:gap-10 desktop:justify-evenly">
       <DatesSelector />
-      {session && (
-        <div className="flex flex-col mb-10">
-          <div className="my-10">
-            <div className="mb-[20px]">최근 생성 이벤트</div>
-            <Link
-              href={`/meet/${myManagingMeets[myManagingMeets.length - 1].id}`}
-            >
-              <MainCard
-                meetName={myManagingMeets[myManagingMeets.length - 1].name}
-                description={
-                  myManagingMeets[myManagingMeets.length - 1].description
-                }
-                startTime={
-                  myManagingMeets[myManagingMeets.length - 1].startTime
-                }
-                endTime={myManagingMeets[myManagingMeets.length - 1].endTime}
-                // participants={
-                //   myManagingMeets[myManagingMeets.length - 1].participants
-                // }
-              />
-            </Link>
-          </div>
-          <div>
-            <div className="mb-[20px]">최근 참여 이벤트</div>
-            <Link
-              href={`/meet/${myParticipatingMeets[myParticipatingMeets.length - 1].id}`}
-            >
-              <MainCard
-                meetName={
-                  myParticipatingMeets[myParticipatingMeets.length - 1].name
-                }
-                description={
-                  myParticipatingMeets[myParticipatingMeets.length - 1]
-                    .description
-                }
-                startTime={
-                  myParticipatingMeets[myParticipatingMeets.length - 1]
-                    .startTime
-                }
-                endTime={
-                  myParticipatingMeets[myParticipatingMeets.length - 1].endTime
-                }
-                // participants={
-                //   myParticipatingMeets[myParticipatingMeets.length - 1].participants
-                // }
-              />
-            </Link>
-          </div>
-        </div>
-      )}
+      {managingMeetsContent}
+      {participatingMeetsContent}
     </div>
   );
 }
