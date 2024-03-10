f2bb8f369d1649792b8c71621779e0fcd1e944c1
Gwon Hong <gwonhong.dev@gmail.com>
2024-03-10 20:29:30 +0900

    add: apply form state at participate form

--- src/app/(main)/meet/[meetId]/participate/ParticipateForm.tsx
+++ src/app/(main)/meet/[meetId]/participate/ParticipateForm.tsx
@@ -1,6 +1,30 @@
 'use client';
 
-import participateFormAction from './participateFormAction';
+import type FormState from '@/types/FormState';
+import { useFormState } from 'react-dom';
+import participateMeetAction from './participateMeetAction';
+import { useEffect, useState } from 'react';
+import LoadingOverlay from '@/components/LoadingOverlay';
+import { useRouter } from 'next/navigation';
+import { SubmitButton } from '@/components/meet-form/SubmitButton';
+
+async function clientAction(
+  prevState: FormState,
+  formData: FormData
+): Promise<FormState> {
+  try {
+    await participateMeetAction(formData);
+    return {
+      state: 'success',
+      error: null,
+    };
+  } catch (error) {
+    return {
+      state: 'error',
+      error: error instanceof Error ? error.message : 'unexpected error',
+    };
+  }
+}
 
 export default function ParticipateForm({
   meetId,
@@ -9,15 +33,35 @@ export default function ParticipateForm({
   meetId: string;
   isProtected: boolean;
 }) {
+  const router = useRouter();
+  const [formState, formAction] = useFormState<FormState, FormData>(
+    clientAction,
+    {
+      state: 'idle',
+      error: null,
+    }
+  );
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(() => {
+    if (formState.state === 'success') {
+      setIsLoading(true);
+      router.push(`/meet/${meetId}`);
+    } else if (formState.state === 'error') {
+      alert(formState.error);
+    }
+  }, [formState]);
   return (
-    <form action={participateFormAction}>
-      <input type="hidden" name="meetId" value={meetId} readOnly />
-      <input
-        type={isProtected ? 'password' : 'hidden'}
-        className="text-black"
-        name="meetPassword"
-      />
-      <button type="submit">참가하기</button>
-    </form>
+    <>
+      {isLoading && <LoadingOverlay />}
+      <form action={formAction}>
+        <input type="hidden" name="meetId" value={meetId} readOnly />
+        <input
+          type={isProtected ? 'password' : 'hidden'}
+          className="text-black"
+          name="meetPassword"
+        />
+        <SubmitButton text="참가하기" />
+      </form>
+    </>
   );
 }
