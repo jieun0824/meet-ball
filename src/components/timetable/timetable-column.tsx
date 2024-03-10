214469664b9105671d6304ff127e18565d4b82f8
jieun <suspen064@gmail.com>
2024-03-10 13:28:20 +0900

    style: fix after class

--- src/components/timetable/timetable-column.tsx
+++ src/components/timetable/timetable-column.tsx
@@ -56,8 +56,9 @@ function TimeTableCell({
       onMouseOver={() => {
         setHoverData(participants);
       }}
-      className={`h-[20px] cursor-pointer border-white ${timeIndex % 2 === 0 ? 'border-t-[0.3px]' : ''}
-      ${previousOpacity === 0 && opacity != 0 && 'rounded-t-lg'} ${nextOpacity === 0 && opacity != 0 && 'rounded-b-lg'} ${clicked && '!bg-white/50 bg-opacity-35 !rounded-none'} `}
+      className={`relative h-[20px] cursor-pointer border-white ${timeIndex % 2 === 0 ? 'border-t-[0.3px]' : ''}
+      ${previousOpacity === 0 && opacity != 0 && 'rounded-t-lg'} ${nextOpacity === 0 && opacity != 0 && 'rounded-b-lg'} 
+      ${clicked && 'after:bg-pink-200 after:bg-opacity-50 after:z-50 after:absolute after:inset-0 after:mx-2'} `}
       onClick={clickHandler}
     ></div>
   );
